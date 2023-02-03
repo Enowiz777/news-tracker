654cc86f7434c8ce910205bba5e49ef7a16658aa
Enoch Park <enoch.park@enowiz.com>
2023-02-02 22:00:21 -0800

    20230202

--- src/App.js
+++ src/App.js
@@ -24,19 +24,18 @@ const TextContainer = styled.div`
 
 const ArticleLink = styled.a``;
 
-
-
 function App() {
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  const APIkey = "pub_16508fbf5b182f5a8c6b3eaa272a8852f15a7";
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=8c63e5b631f3491aa7a9db8d04c969c3');
+      const data = await fetch(`https://newsdata.io/api/1/news?apikey=${APIkey}&country=us,gb`);
       const json = await data.json();
-      console.log(json.articles);
-      setNews(json.articles);
+      const results = json.results;
+      console.log(results);
+      setNews(results);
       setLoading(false);
       
     }
@@ -54,43 +53,21 @@ function App() {
           ): (
           news.map((data) =>
               <Article>
-                <NewsImage src={data.urlToImage}></NewsImage>
+                <NewsImage src={data.image_url}></NewsImage>
                 <TextContainer>
                   <h1>{data.title}</h1>
-                  <h3>{data.author}</h3>
+                  <h3>{data.source_id} | {data.pubDate}</h3>
                   <p>{data.description}</p>
-                  <a target="_blank" href={data.url}>Read</a>
+                  {/* <a target="_blank" href={data.url}>Read</a> */}
 
                 </TextContainer>
                 {/* <img src={data.urlToImage}></img> */}
               </Article>
           ))
         }
-        <div class="jumbotron">
-          <h1 class="display-4">Hello, world!</h1>
-          <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-          <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-        </div>
+
     </>
   );
 }
-
-/*
-      loading ? {
-          <h1>Data is loading...</h1>
-      }: {
-          news.map((data) =>
-            {
-              <div>
-                <h1>data.title</h1>
-                <h3>data.author</h3>
-                <p>data.content</p>
-              </div>
-            }
-          )
-      }
- 
-*/
 
 export default App;
